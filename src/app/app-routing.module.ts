ae5eeb5ee6f1a58a49ccc66c16deff3a28f2e200
Joaquin Diaz <joacod@gmail.com>
2020-04-24 15:51:10 +0200

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LyricsComponent } from './lyrics/lyrics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lyrics', component: LyricsComponent },
+  { path: '', redirectTo: 'lyrics', pathMatch: 'full' },
+  { path: '**', redirectTo: 'lyrics', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
